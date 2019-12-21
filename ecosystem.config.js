25185e6ff4b0528163bd61fc4eef31d6f03e1b4c
Marco Caggiano <mcisback@gmail.com>
2019-12-21 03:38:47 +0100

    +ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    apps : [{
+      name: 'StatusMeteo',
+      script: 'server.js',
+  
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      args: '',
+      instances: 1,
+      autorestart: true,
+      watch: true,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'dev'
+      },
+      env_production: {
+        NODE_ENV: 'prod'
+      }
+    }],
+  
+    deploy : {
+      production : {
+        user : 'web',
+        host : 'localhost',
+        ref  : 'origin/master',
+        repo : 'git@github.com:mcisback/statusmeteo.git',
+        path : '/home/web/web/webapps/jsstatusmeteo',
+        'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      }
+    }
+  };
