42c92246dc00c96a3f9f41176dddc211c34367bd
Marco Caggiano <mcisback@gmail.com>
2019-12-27 06:17:07 +0100

    +runpm2.sh

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: true,
-      max_memory_restart: '1G',
+      max_memory_restart: '1.5G',
       env: {
         NODE_ENV: 'dev'
       },
--- runmp2.sh
+++ runmp2.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-pm2 start ecosystem.config.js --env production
+if [ "$1" -eq "start" ]; then
+
+    # START
+    pm2 start ecosystem.config.js --env production
+
+elif [ "$1" -eq "reload" ]; then
+
+    # RELOAD
+    pm2 reload ecosystem.config.js --env production
+
+fi
